59e5984d7d1987c4b765c0ee68fee26b48a18d92
AlastorCarmesi <carmesiblog7@gmail.com>
2022-10-05 18:45:12 -0500

    Creacion de servidor 05/Oct/2022

--- SRC/app.js
+++ SRC/app.js
@@ -1,1 +1,14 @@
 const express = require('express');
+const app = express();
+
+const http = require('http');
+const server = http.createServer(app);
+
+app.get('/',(req,res)=>{
+    res.send('<h1>Hola mundo</h1>')
+});
+
+server.listen(3000, ()=>{
+    console.log('servidor funcionando')
+});
+
